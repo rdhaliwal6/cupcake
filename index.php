d61dd05aad93da2da2455e7b88d408052b8e18c3
Rajpreet Dhaliwal <Rajpreet9910@gmail.com>
2020-01-13 21:13:59 -0500

    Added bootstrap & jquery

--- index.php
+++ index.php
@@ -7,15 +7,20 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Bootstrap & Pizza CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="style.css">
     <link rel="script" href="script.js">
     <title>Cupcake Fundraiser</title>
 </head>
 <body>
+    <div class="container" id="main">
     <h1>Cupcake Fundraiser</h1>
     <div class="form-group">
         <label for="name">Your Name:</label><br>
-    <input type="text" class="form-control" id="name" >
+    <input type="text" class="form-control" id="name"
+           placeholder="Please input your name.">
     <span class="err" id="err-name">
                 Please enter a Name
     </span>
@@ -23,8 +28,15 @@
 
     <form id="cupcake-form" action="confirmation.php" method="post">
         <button id="submit" type="submit" class="btn btn-primary">
-            Submit your order
+            Order
         </button>
     </form>
+    </div>
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
 </body>
 </html>
