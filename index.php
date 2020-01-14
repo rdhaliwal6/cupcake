b69ef2f40baef4c89c0d886d93de212cb34a6c30
Rajpreet Dhaliwal <Rajpreet9910@gmail.com>
2020-01-13 20:35:00 -0500

    Added name input

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 ?>
-<<!doctype html>
+<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -10,6 +10,19 @@
     <title>Cupcake Fundraiser</title>
 </head>
 <body>
+    <h1>Cupcake Fundraiser</h1>
+    <div class="form-group">
+        <label for="name">Your Name:</label><br>
+    <input type="text" class="form-control" id="name" >
+    <span class="err" id="name">
+                Please enter a Name
+    </span>
+    </div>
 
+    <form id="cupcake-form" action="confirmation.php" method="post">
+        <button id="submit" type="submit" class="btn btn-primary">
+            Submit your order
+        </button>
+    </form>
 </body>
 </html>
